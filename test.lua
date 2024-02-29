2875b2111a043a25122204299385782facdfd3a7
Gentoolman <146978592+WH0LEWHALE@users.noreply.github.com>
2024-03-01 00:41:46 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,60 +1,60 @@
+--[[ VARS & FUNCTIONS ]]--
+---------------------------------------------------------------------
+
 io.write('Enter a number please, \n')
 local f = io.read("n")
---[[
-NOT NEEDED!
+local var = f
 
-io.write('Your number is ', f, '!\n')
+local contents = {"Hmm, weird to see this text file isnt it?", "hmm", "testing", "bla, bla, bla bla bla.", "WH0LEWHALE Presents...", "Spiders", "Fellas!", "Sorry if your pc blowed.", "Tranquila Baby!!!!", "Dont worry!", "1", "2", "3", "YOOO", "VSauce Michael Here.", "Bloooat"}
 
-]]--
+error = [[Your input number needs to lower or higher than ]]
+bracket = [["]]
 
 function wait(n)
   os.execute("sleep " .. tonumber(n))
 end
 
-error = [[Your input number needs to lower or higher than ]]
-bracket = [["]]
+---------------------------------------------------------------------
+--[[ VARS & FUNCTIONS ]]--
 
-local var = f
-
-local contents = {"Hmm, weird to see this text file isnt it?", "hmm", "testing", "bla, bla, bla bla bla.", "WH0LEWHALE Presents...", "Spiders", "Fellas!", "Sorry if your pc blowed.", "Tranquila Baby!!!!", "Dont worry!"}
+--[[ MAIN CODE ]]--
+---------------------------------------------------------------------
 
 if var == 5 then
-print("ur welcome kid.")
+    print("ur welcome kid.")
 elseif var == 10 then
-print("wow ur so lucky")
+    print("wow ur so lucky")
 elseif var <= 1 then
-wait(1)
-print("\n5")
-wait(1)
-print("4")
-wait(1)
-print("3")
-wait(1)
-print("2")
-wait(1)
-print("1")
-wait(1)
-os.exit()
+  wait(1)
+    print("\n5")
+  wait(1)
+    print("4")
+  wait(1)
+    print("3")
+  wait(1)
+    print("2")
+  wait(1)
+    print("1")
+  wait(1)
+    os.exit()
 elseif var == 15 then
-file = io.open("testing" .. random .. ".txt", "w")
-file:write(contents[math.random( #contents )])
-file:close()
-print("Look at your files, i've created a .txt file.")
+    file = io.open("testing" .. random .. ".txt", "w")
+    file:write(contents[math.random( #contents )])
+    file:close()
+    print("Look at your files, i've created a .txt file.")
 elseif var >= 25 then
-print("YOU WANT MORE? OKAY ILL DO IT.")
-wait(1)
+    print("YOU WANT MORE? OKAY ILL DO IT.")
+    wait(1)
 for i = 0, 150, 1 do
-print("COPYING FILES...")
-for i = 0, 150, 1 do
-num = math.random(0, 150)
-file = io.open("./sat/grah" .. num .. ".txt", "w")
-file:write(contents[math.random( #contents )])
+  print("COPYING FILES...")
+  num = math.random(0, 150)
+    file = io.open("./sat/grah" .. num .. ".txt", "w")
+    file:write(contents[math.random( #contents )])
 end
-end
-print("Hope you're happy now, you greedy.")
+    print("Hope you're happy now, you greedy.")
 else
-print(error .. bracket .. var .. bracket)
+    print(error .. bracket .. var .. bracket)
 end
---[[
-This is my example script, dont take serious.
-]]--
+
+---------------------------------------------------------------------
+--[[ MAIN CODE ]]--
