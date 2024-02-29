0e16b74978400caaf7eba63a16913c74d36f347e
Gentoolman <146978592+WH0LEWHALE@users.noreply.github.com>
2024-02-29 23:49:36 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -15,16 +15,13 @@ error = [[Your input number needs to lower or higher than ]]
 bracket = [["]]
 
 local var = f
-random = math.random(1, 30)
 
-local contents = {"Hmm, weird to see this text file isnt it?", "hmm", "testing", "bla, bla, bla bla bla.", "WH0LEWHALE Presents...", "Spiders", "Fellas!"}
+local contents = {"Hmm, weird to see this text file isnt it?", "hmm", "testing", "bla, bla, bla bla bla.", "WH0LEWHALE Presents...", "Spiders", "Fellas!", "Sorry if your pc blowed.", "Tranquila Baby!!!!", "Dont worry!"}
 
 if var == 5 then
 print("ur welcome kid.")
 elseif var == 10 then
 print("wow ur so lucky")
-elseif var == 15 then
-print("stay out!")
 elseif var <= 1 then
 wait(1)
 print("\n5")
@@ -38,15 +35,25 @@ wait(1)
 print("1")
 wait(1)
 os.exit()
-elseif var > 15 then
+elseif var == 15 then
 file = io.open("testing" .. random .. ".txt", "w")
 file:write(contents[math.random( #contents )])
 file:close()
 print("Look at your files, i've created a .txt file.")
+elseif var >= 25 then
+print("YOU WANT MORE? OKAY ILL DO IT.")
+wait(1)
+for i = 0, 150 do
+num = math.random() and math.random() and math.random() and math.random(0, 150)
+print("COPYING FILES...")
+file = io.open("testing" .. num .. ".txt", "w")
+file:write(contents[math.random( #contents )])
+file:close()
+end
+print("Hope you're happy now, you greedy.")
 else
 print(error .. bracket .. var .. bracket)
 end
-
 --[[
 This is my example script, dont take serious.
 ]]--
